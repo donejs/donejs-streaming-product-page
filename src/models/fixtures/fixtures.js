e52cb9da6e10f55eeffb1206d31a2052c9187ad7
Matthew Phillips <matthew@matthewphillips.info>
2017-07-26 09:54:47 -0400

    remove unused stuff

--- src/models/fixtures/fixtures.js
+++
@@ -1,5 +0,0 @@
-import 'donejs-streaming-product-page/models/fixtures/products';
-
-import 'donejs-streaming-product-page/models/fixtures/carts';
-
-// Main file that loads all model fixtures
--- src/models/test.js
+++
@@ -1,5 +0,0 @@
-import './fixtures/';
-
-import 'donejs-streaming-product-page/models/product-test';
-
-import 'donejs-streaming-product-page/models/cart-test';
